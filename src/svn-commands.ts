09ad07758540a8366db4d812cd9ef652d0342baf
skolanos <skolanos77@gmail.com>
2017-06-18 19:48:22 +0200

    Dodałem ikony ze satusami do widoku Source Control

--- src/svn-commands.ts
+++ src/svn-commands.ts
@@ -1,5 +1,6 @@
 import { Disposable, commands, window, Uri } from 'vscode';
 import * as child_process from 'child_process';
+import * as path from 'path';
 
 import { SvnSourceControl } from './svn-source-control';
 
@@ -23,14 +24,12 @@ export class SvnCommands {
 					window.showErrorMessage(`Wystąpił bład w poleceniu vscode-svn.status: ${stderr}`);
 					return;
 				}
-				/* DEBUG:
-				stdout = '12345678./test-status.txt\n12345678./src/app/main-status.js\n12345678./src/stat-status.md';
-				*/
+				// DEBUG: stdout = 'M2345678./test-status.txt\n?2345678./src/app/main-status.js\n?2345678./src/stat-status.md';
 				let elements: string[] = stdout.split('\n');
 				this.svnSourceControl.getChangesTree().resourceStates = elements.map((element: string) => {
 					let attr = element.substr(0, 8);
 					let fileName = element.substr(8).trim();
-					return { resourceUri: new Uri().with({ path: fileName }) };
+					return { resourceUri: new Uri().with({ path: fileName }), decorations: {iconPath: Uri.file(this.getPathForFileStateIcon(attr))} };
 				});
 			});
 		}));
@@ -38,12 +37,15 @@ export class SvnCommands {
 			window.showInformationMessage('To jest polecenie vscode-svn.diff');
 			/*
 			this.svnSourceControl.getChangesTree().resourceStates = [
-				{ resourceUri: new Uri().with({ path: './test-status.txt' }) },
+				{ resourceUri: new Uri().with({ path: './test-status.txt' }), decorations: {iconPath: Uri.file(this.getPathForFileStateIcon('C2345678')) } },
 				{ resourceUri: new Uri().with({ path: './src/app/main-status.js' }) },
 				{ resourceUri: new Uri().with({ path: './src/stat-status.md' }) },
 				{ resourceUri: new Uri().with({ path: './src/lib' }) }
 			];
 			*/
+		}));
+		this.disposables.push(commands.registerCommand('vscode-svn.update-all', () => {
+			window.showInformationMessage('To jest polecenie vscode-svn.update-all');
 		}));
 		this.disposables.push(commands.registerCommand('vscode-svn.update', () => {
 			window.showInformationMessage('To jest polecenie vscode-svn.update');
@@ -51,6 +53,26 @@ export class SvnCommands {
 		this.disposables.push(commands.registerCommand('vscode-svn.commit', () => {
 			window.showInformationMessage('To jest polecenie vscode-svn.commit');
 		}));
+	}
+	getPathForFileStateIcon(fileState: string): string {
+		let iconName = '';
+
+		switch (fileState.substr(0, 1)) {
+		case ' ': iconName = 'stat01.png'; break;
+		case 'A': iconName = 'stat02.png'; break;
+		case 'D': iconName = 'stat03.png'; break;
+		case 'M': iconName = 'stat04.png'; break;
+		case 'R': iconName = 'stat05.png'; break;
+		case 'C': iconName = 'stat06.png'; break;
+		case 'X': iconName = 'stat07.png'; break;
+		case 'I': iconName = 'stat08.png'; break;
+		case '?': iconName = 'stat09.png'; break;
+		case '!': iconName = 'stat10.png'; break;
+		case '~': iconName = 'stat11.png'; break;
+		default:  iconName = 'stat01.png';
+		}
+
+		return path.join(__dirname, '..', '..', 'resources', 'icons', 'dark', iconName);
 	}
 	dispose(): void {
 		this.disposables.forEach((disposable: Disposable) => {
